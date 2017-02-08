8ac9da2db85d821a6eae0809a947258132e40ecc
tim-hub <tim_w@outlook.com>
2017-02-08 21:04:59 +1300

    update client generals.py

--- generals_io_client/generals.py
+++ generals_io_client/generals.py
@@ -66,21 +66,13 @@ class Generals(object):
 
     def move(self, y1, x1, y2, x2, move_half=False):
         if not self._seen_update:
-            raise ValueError("Cannot move_to before first map seen")
+            raise ValueError("Cannot move before first map seen")
 
         cols = self._map[0]
         a = y1 * cols + x1
         b = y2 * cols + x2
         self._send(["attack", a, b, move_half, self._move_id])
         self._move_id += 1
-
-    # # more original version move
-    # def move(self, a,b, move_half=False):
-    #     if not self._seen_update:
-    #         raise ValueError("Cannot move_to before first map seen")
-    #
-    #     self._send(["attack", a, b, move_half, self._move_id])
-    #     self._move_id += 1
 
     def get_updates(self):
         while True:
@@ -110,10 +102,9 @@ class Generals(object):
                 logging.info("Game Prepare to Start")
             elif msg[0] == "game_start":
                 logging.info("Game info: {}".format(msg[1]))
-                # logging.info(self._REPLAY_URLS[self._region]+ self._start_data['replay_id'])
                 self._start_data = msg[1]
             elif msg[0] == "game_update":
-                yield self._make_update(msg[1]) #mag[1] is data
+                yield self._make_update(msg[1])
             elif msg[0] in ["game_won", "game_lost"]:
                 yield self._make_result(msg[0], msg[1])
                 break
@@ -124,13 +115,8 @@ class Generals(object):
         self._ws.close()
 
     def _make_update(self, data):
-        # logging.info(data['turn'])
         _apply_diff(self._map, data['map_diff'])
-        # logging.info('map diff: %s and count: %s' %( data['map_diff'], len(data['map_diff']) ))
         _apply_diff(self._cities, data['cities_diff'])
-        # logging.info('cities diff: %s and count: %s' %( data['cities_diff'],len(data['cities_diff'] )))
-        # logging.info(self._map)
-
         if 'stars' in data:
             self._stars[:] = data['stars']
 
@@ -144,9 +130,6 @@ class Generals(object):
             'complete': False,
             'rows': rows,
             'cols': cols,
-            'height': rows,
-            'width': cols,
-
             'player_index': self._start_data['playerIndex'],
             'turn': data['turn'],
             'army_grid': [[self._map[2 + y*cols + x]
@@ -155,54 +138,17 @@ class Generals(object):
             'tile_grid': [[self._map[2 + cols*rows + y*cols + x]
                           for x in range(cols)]
                           for y in range(rows)],
-            # 'lands': [s['tiles'] for s in scores],
-            # 'armies': [s['total'] for s in scores],
-            # 'alives': [not s['dead'] for s in scores],
+            'lands': [s['tiles'] for s in scores],
+            'armies': [s['total'] for s in scores],
+            'alives': [not s['dead'] for s in scores],
             'generals': [(-1, -1) if g == -1 else (g // cols, g % cols)
                          for g in data['generals']],
             'cities': [(c // cols, c % cols) for c in self._cities],
             'usernames': self._start_data['usernames'],
             'teams': self._start_data.get('teams'),
             'stars': self._stars,
-<<<<<<< HEAD:generals_io_client/generals.py
-            'replay_url': (_REPLAY_URLS[self._region]
-                           + self._start_data['replay_id']),
-
-
-            'score_lands': [s['tiles'] for s in scores],
-            'score_armies': [s['total'] for s in scores],
-            'score_alives': [not s['dead'] for s in scores],
-            # return the index format data
-            'armies': self._map[2:cols*rows+1],
-            'terrain' : self._map[cols*rows+2:len(self._map)-1]
-=======
             'replay_url': _REPLAY_URL + self._start_data['replay_id'],
->>>>>>> 61da538... Remove support for NA and EU regions:generals.py
         }
-        # return {
-        #     'complete': False,
-        #     'rows': rows,
-        #     'cols': cols,
-        #     'player_index': self._start_data['playerIndex'],
-        #     'turn': data['turn'],
-        #     'army_grid': [[self._map[2 + y*cols + x]
-        #                   for x in range(cols)]
-        #                   for y in range(rows)],
-        #     'tile_grid': [[self._map[2 + cols*rows + y*cols + x]
-        #                   for x in range(cols)]
-        #                   for y in range(rows)],
-        #     'lands': [s['tiles'] for s in scores],
-        #     'armies': [s['total'] for s in scores],
-        #     'alives': [not s['dead'] for s in scores],
-        #     'generals': [(-1, -1) if general == -1 else (general // cols, general % cols)
-        #                  for general in data['generals']],
-        #     'cities': [(c // cols, c % cols) for c in self._cities],
-        #     'usernames': self._start_data['usernames'],
-        #     'teams': self._start_data.get('teams'),
-        #     'stars': self._stars,
-        #     'replay_url': (_REPLAY_URLS[self._region]
-        #                    + self._start_data['replay_id']),
-        # }
 
     def _make_result(self, update, data):
         return {
