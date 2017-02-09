67a4171c33f373a438840393156138f58347b1c6
tim-hub <tim_w@outlook.com>
2017-02-09 20:07:35 +1300

    alter console output

--- console_output.py
+++ console_output.py
@@ -1,7 +1,14 @@
 import os
+from generals_io_client.generals import FOG,EMPTY,OBSTACLE,MOUNTAIN
 
 def clear():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def print_with_clear():
+def output(state, **kwargs):
     clear()
+    #terrain /tiles
+    terrain=state['tile_grid']
+    armies=state['army_grid']
+    pi=state['player_index']
+
+
